695897b3e2c8347e87533d176022e0fcb0d9fd91
LonerDeni <124445115+LonerDeni@users.noreply.github.com>
2023-10-16 13:55:21 +0300

    add jpa-repositories query

--- src/main/java/com/example/hibernatedao/repositories/PersonRepositories.java
+++ src/main/java/com/example/hibernatedao/repositories/PersonRepositories.java
@@ -2,6 +2,8 @@ package com.example.hibernatedao.repositories;
 
 import com.example.hibernatedao.model.Persons;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,8 +11,12 @@ import java.util.Optional;
 
 @Repository
 public interface PersonRepositories extends JpaRepository<Persons,Long> {
-    List<Persons> findByCityOfLiving(String city);
+    @Query(value = "select e from Persons e where upper(e.cityOfLiving) = upper(:city)")
+    List<Persons> findByCityOfLiving(@Param("city") String city);
 
-    List<Persons> findByAgeLessThanOrderByAgeDesc(int age);
-    Optional<Persons> findByNameIgnoreCaseAndSurNameIgnoreCase(String name, String surName);
+    @Query(value = "select e from Persons e where e.age < :age order by e.age asc")
+    List<Persons> findByAgeLessThanOrderByAgeDesc(@Param("age") int age);
+
+    @Query(value = "select e from Persons e where upper(e.name) = upper(:name) and upper(e.surName) = upper(:surname)")
+    Optional<Persons> findByNameIgnoreCaseAndSurNameIgnoreCase(@Param("name") String name, @Param("surname") String surName);
 }
